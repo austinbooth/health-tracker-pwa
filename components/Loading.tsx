4496c052bb36b08d926065c21c87920125315c0d
Austin Booth <austinbooth78@gmail.com>
2022-09-07 22:54:08 +0100

    fixes Vercel lint error

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,7 +1,9 @@
 import CircularProgress from '@mui/material/CircularProgress'
 
-export default () => (
+export default function Loading() {
+  return (
   <div className="flex flex-col justify-center items-center py-16 h-screen">
     <CircularProgress />
   </div>
-)
+  )
+}
