42302329adb5e06d099700cb8ca8334e481de381
Austin Booth <austinbooth78@gmail.com>
2024-01-15 23:02:07 +0000

    Add settings and files required for a PWA

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,23 @@ import { Providers } from './Providers'
 export const metadata: Metadata = {
   title: 'Health Tracker',
   description: 'Track your weight etc',
+  generator: "Next.js",
+  manifest: "/manifest.json",
+  keywords: ["nextjs", "nextjs13", "next13", "pwa", "next-pwa"],
+  themeColor: [{ media: "(prefers-color-scheme: dark)", color: "#fff" }],
+  authors: [
+    { name: "Austin Booth" },
+    {
+      name: "Austin Booth",
+      url: "https://www.github.com/austinbooth/",
+    },
+  ],
+  viewport:
+    "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
+  icons: [
+    { rel: "apple-touch-icon", url: "icons/128.png" },
+    { rel: "icon", url: "icons/128.png" },
+  ],
 }
 
 export default function RootLayout({
